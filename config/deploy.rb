0966b6555b6ae9349fb12d536a1861e430207536
岩永規秀 <motohide@iwanagatadashishuus-MacBook-Pro.local>
2017-10-31 19:51:58 +0900

    Add-capfile
    Capファイルの導入。

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.10.0"
+
+set :application, "booklog16d"
+set :repo_url, "git@github.com:Motohide/booklog16d.git"
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.1'
+
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['/Users/motohide/.ssh/key\pair.pem']
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,1 @@
+server '13.115.168.63', user: 'ec2-user', roles: %w{app db web}
