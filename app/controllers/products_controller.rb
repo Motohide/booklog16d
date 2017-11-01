b08fe2b1676e7200473a4fa7aa1e9448859d7d37
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-11-01 19:31:24 +0900

    Define actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,8 @@
 class ReviewsController < ApplicationController
+
+  def new
+  end
+
+  def edit
+  end
 end
