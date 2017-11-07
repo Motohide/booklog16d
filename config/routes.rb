783bf14fb729de160f7c4f0beca16d3739d2459d
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-11-07 20:22:31 +0900

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
       get 'my_books'
     end
   end
-  resources :products, only: [:index, :new, :show] do
+  resources :products, only: [:index, :new, :create, :show] do
     resources :reviews, only: [:new, :edit]
     collection do
       get 'search'
