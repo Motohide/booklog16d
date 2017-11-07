ede2b636fbbef2028373a05938d57bfc566b9512
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-11-07 20:23:35 +0900

    Edit controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,11 +4,24 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to :root
+    end
   end
 
   def show
   end
 
   def search
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name, :image, :author, :pulisher, :release_date)
   end
 end
