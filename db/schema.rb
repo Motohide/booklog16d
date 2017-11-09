7cb9eb8001463504b6a0d8b2b3dfdde896edc4c8
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-11-09 16:53:42 +0900

    Change columns of User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109055729) do
+ActiveRecord::Schema.define(version: 20171109070954) do
 
   create_table "bookshelves", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -64,9 +64,6 @@ ActiveRecord::Schema.define(version: 20171109055729) do
     t.datetime "updated_at",                          null: false
     t.string   "nickname"
     t.integer  "gender"
-    t.integer  "birth_year"
-    t.integer  "birth_month"
-    t.integer  "birth_day"
     t.string   "avatar"
     t.integer  "prefecture"
     t.string   "hometown"
@@ -74,6 +71,7 @@ ActiveRecord::Schema.define(version: 20171109055729) do
     t.string   "biography"
     t.string   "job"
     t.boolean  "admin"
+    t.date     "birth_date"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["nickname"], name: "index_users_on_nickname", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
