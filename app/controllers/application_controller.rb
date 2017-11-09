f0e05e6879f131ff9c24499ccd4dcb0c4271ac57
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-11-09 15:12:14 +0900

    Set keys

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
+def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :gender, :birth_year, :birth_month, :birth_day, :avatar, :prefecture, :hometown, :website, :biography,:job])
+  devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :gender, :birth_year, :birth_month, :birth_day, :avatar, :prefecture, :hometown, :website, :biography,:job])
 end
