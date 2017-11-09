12430383782c81ec950c05f65246ddd33bee3236
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-11-09 20:21:03 +0900

    Define edit and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,21 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+      if @user.update(update_params)
+        redirect_to :root, notice: 'プロフィールを編集しました'
+      else
+        render 'edit'
+      end
   end
+
+  private
+  def update_params
+    params.require(:user).permit(:email, :nickname, :gender, :birth_date, :avatar, :prefecture_code, :hometown, :website, :biography, :job)
+  end
+
 end
