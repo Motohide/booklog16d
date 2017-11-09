452ff1829bf3ccfcaa257c29cbc504c601f53c10
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-11-09 16:07:33 +0900

    Change input into form_for

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
-def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :gender, :birth_year, :birth_month, :birth_day, :avatar, :prefecture, :hometown, :website, :biography,:job])
-  devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :gender, :birth_year, :birth_month, :birth_day, :avatar, :prefecture, :hometown, :website, :biography,:job])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :gender, :birth_year, :birth_month, :birth_day, :avatar, :prefecture, :hometown, :website, :biography,:job])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :gender, :birth_year, :birth_month, :birth_day, :avatar, :prefecture, :hometown, :website, :biography,:job])
+  end
 end
