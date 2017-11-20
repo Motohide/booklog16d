5ba2d7be439787d561f196641732d3aef4c56e7c
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-11-20 19:45:09 +0900

    Create netabare for review in product page

---
+++ app/assets/javascripts/spoiler.js
@@ -0,0 +1,10 @@
+$(document).on('turbolinks:load',function(){
+  var $netabare = $('.review-area__summary[spoiler_status=1]');
+  $netabare.find('.review-area__summary__reaction').before('<span class="netabare_mark"><img src="/assets/netabare_mark.png"></span>');
+  $netabare.find('.review-area__summary__review-text p').addClass('prepend-spoiler');
+  $netabare.on('mouseover', function(){
+    $('.prepend-spoiler').css('color','#000');
+  }).on('mouseleave', function(){
+    $('.prepend-spoiler').css('color','#f0f0f0');
+  })
+});
