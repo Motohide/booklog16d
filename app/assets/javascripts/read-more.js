2c4b3bc784dd5debd26fb32703670dda6c828eeb
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-11-21 14:23:57 +0900

    Introduce collapses.js

---
+++ app/assets/javascripts/read-more.js
@@ -0,0 +1,13 @@
+$(function () {
+  $('.review-area__summary__review-text p').collapser({
+    ellipsis: "",
+    mode: 'chars',
+    speed: 'fast',
+    truncate: 120,
+    showText: '</br>続きを読む',
+    hideText: '閉じる',
+    showClass: 'show-class',
+    hideClass: 'hide-class',
+    controlBtn: 'dsfdsf'
+  });
+});
