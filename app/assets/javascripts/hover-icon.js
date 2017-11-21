0c02bf3d7941f6d396134c2e916f43929661008d
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-11-21 20:48:25 +0900

    Set fadeIn, fadeOut

--- app/assets/javascripts/hover-icon.js
+++ app/assets/javascripts/hover-icon.js
@@ -1,19 +1,19 @@
 $(document).on('turbolinks:load', function(){
   $('.global-navi__home').hover(function () {
-        $('.hover-icon', this).fadeIn("3000").css('display','inline');
+        $('.hover-icon', this).fadeIn(500).css('display','inline');
     }, function () {
-        $('.hover-icon', this).fadeOut("3000");
+        $('.hover-icon', this).fadeOut(200);
     });
 
   $('.global-navi__bookshelf').hover(function () {
-        $('.hover-icon', this).fadeIn("3000").css('display','inline');
+        $('.hover-icon', this).fadeIn(500).css('display','inline');
     }, function () {
-        $('.hover-icon', this).fadeOut("3000");
+        $('.hover-icon', this).fadeOut(200);
     });
 
   $('.global-navi__timeline').hover(function () {
-        $('.hover-icon', this).fadeIn("3000").css('display','inline');
+        $('.hover-icon', this).fadeIn(500).css('display','inline');
     }, function () {
-        $('.hover-icon', this).fadeOut("3000");
+        $('.hover-icon', this).fadeOut(200);
     });
 })
