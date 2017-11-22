978552aaf327eebd602298b4cc04127c77c5110a
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-11-22 16:52:18 +0900

    Set Jquery for dropdown-menu

---
+++ app/assets/javascripts/dropdown-menu.js
@@ -0,0 +1,14 @@
+$(document).on('turbolinks:load', function() {
+    $('.global-navi__dropdown-menu-click a').on('click',function(e){
+    e.preventDefault()
+    $(this).parent().find('.dropdown-menu').toggle(300);
+    // Hide menu when clicked outside
+    $(this).parent().find('.dropdown-menu').mouseleave(function(){
+      var thisUI = $(this);
+      $('html').click(function(){
+        thisUI.hide();
+        $('html').unbind('click');
+      });
+    });
+  });
+});
