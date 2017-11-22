d70364b850ef56f729eaddc7e39c892503577fe5
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-11-22 17:18:48 +0900

    Replace logout icon by user icon

--- app/assets/javascripts/dropdown-menu.js
+++ app/assets/javascripts/dropdown-menu.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load', function() {
     $('.global-navi__dropdown-menu-click a').on('click',function(e){
     e.preventDefault()
-    $(this).parent().find('.dropdown-menu').toggle(400);
+    $(this).parent().find('.dropdown-menu').toggle(300);
     // Hide menu when clicked outside
     $(this).parent().find('.dropdown-menu').mouseleave(function(){
       var thisUI = $(this);
