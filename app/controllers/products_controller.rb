6a7f45ccba391d7316b42c5a96a7c7a6c480074d
queq1890 <yujimatsumoto@matsumotohiroshinoMacBook-Pro.local>
2017-11-22 14:39:07 +0900

    Modify products/show.html.haml

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,6 +19,8 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
+    @review_total = @product.reviews.where.not(body: nil).length
+    @average_rate = @product.reviews.average(:rate)
 
     if user_signed_in?
       @review = Review.find_or_create_by(product_id: params[:id], user_id: current_user.id)
