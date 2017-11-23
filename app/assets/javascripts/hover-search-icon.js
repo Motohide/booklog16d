91df82c2da7262d356a12d54099beb1637db663a
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-11-23 12:47:51 +0900

    Create animation for question-icon (when mouseover search contents intro is shown)

---
+++ app/assets/javascripts/hover-search-icon.js
@@ -0,0 +1,13 @@
+$(document).on('turbolinks:load', function() {
+    $('.question-icon').on('mouseover',function(e){
+    e.preventDefault()
+    $(this).parent().find('.search-content').slideDown(300);
+    //メニュー領域外をホバーしたらメニューを閉じる
+    $(this).parent().find('.search-content').mouseout(function(){
+      var thisUI = $(this);
+      $('html').mouseout(function(){
+        thisUI.slideUp(200);
+      });
+    });
+  });
+});
