662123f1c083abfe7f923f58a1f71fec8b322f5a
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-11-24 16:24:47 +0900

    Modify dropdown-menu-icon.js, dropdown-user-icon.js

--- app/assets/javascripts/dropdown-menu-icon.js
+++ app/assets/javascripts/dropdown-menu-icon.js
@@ -1,13 +1,8 @@
 $(document).on('click', function(e) {
-  if(!$(e.target).closest('.dropdown-menu-icon').length && !$(e.target).closest('.global-navi__dropdown-menu-click').length){
-      $('.dropdown-menu-icon').hide();
+  if($(e.target).closest('.global-navi__dropdown-menu-click').length){
+      $('.dropdown-menu-icon').toggle();
   }
-  else if($(e.target).closest('.global-navi__dropdown-menu-click').length){
-    if($('.dropdown-menu-icon').is(':hidden')){
-       $('.dropdown-menu-icon').toggle();
-    }
-    else{
-       $('.dropdown-menu-icon').hide();
-    }
+  else if(!$(e.target).closest('.global-navi__dropdown-menu-click').length){
+      $('.dropdown-menu-icon').hide();
   }
 });
--- app/assets/javascripts/dropdown-user-icon.js
+++ app/assets/javascripts/dropdown-user-icon.js
@@ -1,13 +1,8 @@
 $(document).on('click', function(e) {
-  if(!$(e.target).closest('.dropdown-user-icon').length && !$(e.target).closest('.global-navi__user-icon-header').length){
-      $('.dropdown-user-icon').hide();
-  }
-  else if($(e.target).closest('.global-navi__user-icon-header').length){
-    if($('.dropdown-user-icon').is(':hidden')){
+  if($(e.target).closest('.global-navi__user-icon-header').length){
       $('.dropdown-user-icon').toggle();
-    }
-    else{
-        $('.dropdown-user-icon').hide();
-    }
+  }
+  else if (!$(e.target).closest('.global-navi__user-icon-header').length){
+      $('.dropdown-user-icon').hide();
   }
 });
