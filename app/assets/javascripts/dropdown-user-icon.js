841ae814300448e03ba94ede0e6e6afe0e82284e
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-11-24 11:50:08 +0900

    Edit file dropdown-user-icon.js (when click user-icon drop-down-menu is disappear)

--- app/assets/javascripts/dropdown-user-icon.js
+++ app/assets/javascripts/dropdown-user-icon.js
@@ -1,8 +1,8 @@
 $(document).on('turbolinks:load', function() {
-    $('.global-navi__user-icon-header').on('click',function(){
-    $(this).parent().find('.dropdown-user-icon').slideDown(0);
-    //メニュー領域外をクリックしたらメニューを閉じる
-    $(this).parent().find('.dropdown-user-icon').mouseleave(function(){
+   $(".global-navi__user-icon-header").on("click", function() {
+            $(this).parent().find('.dropdown-user-icon').toggle(0);
+            $(this).toggleClass("active");
+            $(this).parent().find('.dropdown-user-icon').mouseleave(function(){
       var thisUI = $(this);
       $('html').click(function(){
         thisUI.hide();
@@ -11,3 +11,4 @@ $(document).on('turbolinks:load', function() {
     });
   });
 });
+
