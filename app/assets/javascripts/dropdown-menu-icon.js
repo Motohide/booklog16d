ba48630df7125dcdbc836c1130cdf808df44902a
ocha2812 <tech-camp@tech-camp-EX12-001-no-MacBook-Air.local>
2017-11-24 13:00:41 +0900

    Edit dropdown-menu-icon.js, dropdown-user-icon.js( when click icon menu is appear, disappear)

--- app/assets/javascripts/dropdown-menu-icon.js
+++ app/assets/javascripts/dropdown-menu-icon.js
@@ -1,14 +1,31 @@
-$(document).on('turbolinks:load', function() {
-    $('.global-navi__dropdown-menu-click a').on('click',function(e){
-    e.preventDefault()
-    $(this).parent().find('.dropdown-menu-icon').slideDown(0);
-    //メニュー領域外をクリックしたらメニューを閉じる
-    $(this).parent().find('.dropdown-menu-icon').mouseleave(function(){
-      var thisUI = $(this);
-      $('html').click(function(){
-        thisUI.hide();
-        $('html').unbind('click');
-      });
-    });
-  });
+// $(document).on('turbolinks:load', function() {
+//    $(".global-navi__dropdown-menu-click a").on("click", function(e) {
+//     e.preventDefault()
+//       $(this).parent().find('.dropdown-menu-icon').toggle(0);
+//       $(this).toggleClass("active");
+//       $(this).parent().find('.dropdown-menu-icon').mouseleave(function(){
+//       var thisUI = $(this);
+//       $('html').click(function(){
+//         thisUI.hide();
+//         $('html').unbind('click');
+//       });
+//     });
+//   });
+// });
+
+
+
+$(document).on('click', function(e) {
+  e.preventDefault()
+  if(!$(e.target).closest('.dropdown-menu-icon').length && !$(e.target).closest('.global-navi__dropdown-menu-click a').length){
+      $('.dropdown-menu-icon').hide();
+  }
+  else if($(e.target).closest('.global-navi__dropdown-menu-click a').length){
+    if($('.dropdown-menu-icon').is(':hidden')){
+       $('.dropdown-menu-icon').toggle();
+    }
+    else{
+       $('.dropdown-menu-icon').hide();
+    }
+  }
 });
--- app/assets/javascripts/dropdown-user-icon.js
+++ app/assets/javascripts/dropdown-user-icon.js
@@ -1,14 +1,28 @@
-$(document).on('turbolinks:load', function() {
-   $(".global-navi__user-icon-header").on("click", function() {
-            $(this).parent().find('.dropdown-user-icon').toggle(0);
-            $(this).toggleClass("active");
-            $(this).parent().find('.dropdown-user-icon').mouseleave(function(){
-      var thisUI = $(this);
-      $('html').click(function(){
-        thisUI.hide();
-        $('html').unbind('click');
-      });
-    });
-  });
-});
+// $(document).on('turbolinks:load', function() {
+//    $(".global-navi__user-icon-header").on("click", function() {
+//       $(this).parent().find('.dropdown-user-icon').toggle(0);
+//       $(this).toggleClass("active");
+//       $(this).parent().find('.dropdown-user-icon').mouseleave(function(){
+//       var thisUI = $(this);
+//       $('html').click(function(){
+//         thisUI.hide();
+//         $('html').unbind('click');
+//       });
+//     });
+//   });
+// });
 
+
+$(document).on('click', function(e) {
+  if(!$(e.target).closest('.dropdown-user-icon').length && !$(e.target).closest('.global-navi__user-icon-header').length){
+      $('.dropdown-user-icon').hide();
+  }
+  else if($(e.target).closest('.global-navi__user-icon-header').length){
+    if($('.dropdown-user-icon').is(':hidden')){
+      $('.dropdown-user-icon').toggle();
+    }
+    else{
+        $('.dropdown-user-icon').hide();
+    }
+  }
+});
